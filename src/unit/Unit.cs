58815255d3db9e8afed4d846654df5f084d03cac
moe-moe-pupil <1670426821@qq.com>
2022-10-03 17:30:58 +0800

    feat: add unit

---
+++ src/unit/Unit.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Newtonsoft.Json;
+
+/// <summary>
+///   Main unit in the game
+/// </summary>
+public class Unit
+{
+    /// <summary>
+    ///   unit's red heart number
+    /// </summary>
+    public int RedHeart
+}
