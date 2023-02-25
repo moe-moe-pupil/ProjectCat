0a0d7a1bec3f68258b20c5a6e4b80ea554cd393b
moe-moe-pupil <dhtmitmi@gmail.com>
2023-02-26 00:06:31 +0800

    chore: decouple RDKitTools

--- src/unit/Player.cs
+++ src/unit/Player.cs
@@ -6,157 +6,94 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------
 
-using System;
 using Godot;
 using GodotUtilities;
+using RDKitTools.Utils;
 
 /// <summary lang='zh-CN'>
 ///     玩家单位.
 /// </summary>
-public partial class Player : Unit
-{
+public partial class Player : Unit {
     private bool _bodyDirection = false;
     [Node]
     private AnimatedSprite2D _animatedSprite;
-    private bool _isJump = false;
-    private bool _isHoldJump = false;
     private Camera2D _camera;
-    public bool CouldTrans = false;
-    [Node]
-    private Timer _edgeJump, _holdJump, _bufferingJump;
     [Export]
-    private float _power = 0.5f;
+    private double _coyoteJumpTime = 0.07, _holdJumpTime = 0.15, _bufferingJumpTime = 0.2;
+    private SmartTimer<TimerAction> _jumpTimer = new SmartTimer<TimerAction>();
+    private bool _hasJumped = false;
 
-    public override void _Ready()
-    {
+    public void HandleHoldJump(ref Vector2 velocity) {
+        if (this._hasJumped && Input.IsActionPressed("jump") && !this._jumpTimer.IsActionTimeGone(nameof(this._holdJumpTime))) {
+            velocity.Y -= 14 * this.JumpVelocity;
+        }
+
+        if (Input.IsActionJustReleased("jump")) {
+            this._jumpTimer.Stop(nameof(this._holdJumpTime));
+        }
+    }
+
+    public override void _Ready() {
         this.WireNodes();
+        this._jumpTimer.AddAction(this._coyoteJumpTime, nameof(this._coyoteJumpTime), true);
+        this._jumpTimer.AddAction(this._holdJumpTime, nameof(this._holdJumpTime), false);
+        this._jumpTimer.AddAction(this._bufferingJumpTime, nameof(this._bufferingJumpTime), true);
     }
 
-    public async void OnEnter(Node2D node)
-    {
-        GD.Print("OK");
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        var collision = MoveAndCollide(Velocity * new Vector2((float)delta, (float)delta) * _power, true, 0.08f, true);
-        Vector2 velocity = Velocity;
-        if (IsOnFloor() && !Input.IsActionPressed("ui_accept"))
-        {
-            _edgeJump.Start(timeSec: -1);
-            _edgeJump.Paused = true;
-
-            _holdJump.Start(timeSec: -1);
-            _holdJump.Paused = true;
-
-            _bufferingJump.Start(timeSec: -1);
-            _bufferingJump.Paused = true;
+    public override void _PhysicsProcess(double delta) {
+        Vector2 velocity = this.Velocity;
+        if (this.IsOnFloor()) {
+            this._jumpTimer.ResetAllActions();
+            this._hasJumped = false;
+            if (Input.IsActionJustPressed("jump")) {
+                this._hasJumped = true;
+                this._jumpTimer.Start(nameof(this._holdJumpTime));
+            }
         }
-
-        if (Input.IsActionJustReleased("ui_accept"))
-        {
-            _bufferingJump.Start(timeSec: -1);
-            _bufferingJump.Paused = true;
-            _isHoldJump = false;
-        }
-
-        if (IsOnFloor())
-        {
-            _isJump = false;
-        }
-        else
-        {
-            _edgeJump.Paused = false;
-            velocity.Y += Gravity * (float)delta * 130;
-        }
-
-        // 处理边缘跳跃和长按跳跃 成功触发跳跃
-        if (Input.IsActionJustPressed("ui_accept") && (IsOnFloor() || !_edgeJump.IsStopped()))
-        {
-            _holdJump.Paused = false;
-            _isJump = true;
-        }
-
-        if (Input.IsActionJustPressed("ui_accept") && !IsOnFloor() && _edgeJump.IsStopped())
-        {
-            _bufferingJump.Paused = false;
-        }
-
-        if (Input.IsActionJustPressed("restart") && !IsOnFloor() && _edgeJump.IsStopped())
-        {
-            GetTree().ReloadCurrentScene();
-        }
-
-        if (Input.IsActionPressed("ui_accept") && IsOnFloor() && !_bufferingJump.IsStopped() && !_isHoldJump)
-        {
-            _isJump = true;
-            _holdJump.Start(timeSec: -1);
-            _holdJump.Paused = false;
-            _isHoldJump = true;
-        }
-
-        // 删除IsOnFloor()
-        if (Input.IsActionPressed("ui_accept") && !_holdJump.IsStopped() && _isJump)
-        {
-            velocity.Y -= 14 * JumpVelocity;
-        }
-
-        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-
-        if (inputDir.X > 0)
-        {
-            _animatedSprite.FlipH = false;
-            _bodyDirection = false;
-        }
-        else if (inputDir.X < 0)
-        {
-            _animatedSprite.FlipH = true;
-            _bodyDirection = true;
-        }
-        else
-        {
-            _animatedSprite.FlipH = _bodyDirection;
-        }
-
-        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-        {
-            velocity.X = direction.X * MoveSpeed * 100;
-        }
-        else if (IsOnFloor())
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, MoveSpeed * 20);
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, MoveSpeed * 10);
-        }
-
-        if (velocity.X == 0)
-        {
-            _animatedSprite.Play("Idle");
-        }
-        else
-        {
-            _animatedSprite.Play("Run");
-        }
-
-        if (collision != null)
-        {
-            if (((Node)collision.GetCollider()).Name == "Terrian")
-            {
-                float dirVec = collision.GetNormal().Dot(new Vector2(0, -1));
-                if (dirVec > 0.8f && velocity.Y > 500)
-                {
-                    TileMap terrain = (TileMap)collision.GetCollider();
-                    Vector2I cell = terrain.LocalToMap(collision.GetPosition() - collision.GetNormal());
-                    terrain.SetCell(0, cell, 0);
-                    velocity.Y += Gravity * (float)delta * 130;
-                }
+        else {
+            this._jumpTimer.Update(delta, false);
+            velocity.Y += this.Gravity * (float)delta * 130;
+            if (Input.IsActionJustPressed("jump") && !this._jumpTimer.IsActionTimeGone(nameof(this._coyoteJumpTime))) {
+                this._hasJumped = true;
+                this._jumpTimer.Start(nameof(this._holdJumpTime));
             }
         }
 
-        Velocity = velocity;
-        MoveAndSlide();
+        this.HandleHoldJump(ref velocity);
+
+        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+        if (inputDir.X > 0) {
+            this._animatedSprite.FlipH = false;
+            this._bodyDirection = false;
+        }
+        else if (inputDir.X < 0) {
+            this._animatedSprite.FlipH = true;
+            this._bodyDirection = true;
+        }
+        else {
+            this._animatedSprite.FlipH = this._bodyDirection;
+        }
+
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero) {
+            velocity.X = direction.X * this.MoveSpeed * 100;
+        }
+        else if (this.IsOnFloor()) {
+            velocity.X = Mathf.MoveToward(this.Velocity.X, 0, this.MoveSpeed * 20);
+        }
+        else {
+            velocity.X = Mathf.MoveToward(this.Velocity.X, 0, this.MoveSpeed * 10);
+        }
+
+        if (velocity.X == 0) {
+            this._animatedSprite.Play("Idle");
+        }
+        else {
+            this._animatedSprite.Play("Run");
+        }
+
+        this.Velocity = velocity;
+        this.MoveAndSlide();
     }
 }
